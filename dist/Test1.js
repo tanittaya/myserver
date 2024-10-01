99f86c3e58377713db015730533f748586a9e8c5
tanittaya <152164438+tanittaya@users.noreply.github.com>
2024-10-01 14:14:27 +0700

    done

--- dist/Test1.js
+++ dist/Test1.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
 if (Utils_1.Utils.add(1, 2) === 3) {
-    console.log(1);
+    console.log(0);
 }
 else {
-    console.log(0);
+    console.log(1);
 }
