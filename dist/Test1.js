986c71455e8e5db55f6bfc339fd11e1963a4ed7e
tanittaya <152164438+tanittaya@users.noreply.github.com>
2024-10-09 18:07:28 +0700

    Project

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,50 +1,42 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
-const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
-    //test1
-    if (Utils_1.Utils.add(1, 2) === 3) {
-        console.log(0);
-    }
-    else {
-        console.log('add test1');
-        return;
-    }
-    //test2
-    if (Utils_1.Utils.add(1, 2) == 4) {
-        console.log(0);
-    }
-    else {
-        console.log('add test2');
-        return;
-    }
-    //teest adduser
-    const data = {
-        "name": "pop",
-        "username": "impop"
-    };
-    try {
-        const response = yield Utils_1.Utils.addUser(data);
-        const correct_result = { "name": "pop", "username": "iampop" };
-        const keys = Object.keys(correct_result);
-        for (const k of keys) {
-            if (correct_result[k] !== response[k]) {
-                console.log("addUser" + k);
-                return;
-            }
-        }
-    }
-    catch (error) {
-        console.log("addUser" + error);
-    }
-});
-unit_test();
+let allTestsPassed = true;
+// Test 1: Check if 1 + 1 equals 2
+if (Utils_1.Utils.calculate(1, 1, 1) === '2') {
+    console.log(0); // Test passed
+}
+else {
+    console.log(1001); // Test ID for failure
+    allTestsPassed = false;
+}
+// Test 2: Check if 5 - 3 equals 2
+if (Utils_1.Utils.calculate(5, 3, 4) === '2') {
+    console.log(0); // Test passed
+}
+else {
+    console.log(1002); // Test ID for failure
+    allTestsPassed = false;
+}
+// Test 3: Check if 4 * 2 equals 8
+if (Utils_1.Utils.calculate(4, 2, 8) === '8') {
+    console.log(0); // Test passed
+}
+else {
+    console.log(1003); // Test ID for failure
+    allTestsPassed = false;
+}
+// Test 4: Check if 10 / 2 equals 5
+if (Utils_1.Utils.calculate(10, 2, 2) === '5') {
+    console.log(0); // Test passed
+}
+else {
+    console.log(1004); // Test ID for failure
+    allTestsPassed = false;
+}
+// Final check to print failure message if any test failed
+if (!allTestsPassed) {
+    console.log(1005); // General failure code for at least one failed test
+}
+// Optionally, exit the process with the appropriate code
+process.exit(allTestsPassed ? 0 : 1);
--- dist/Utils.js
+++ dist/Utils.js
@@ -1,28 +1,15 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Utils = void 0;
-const axios_1 = __importDefault(require("axios"));
-function add(a, b) {
-    return a + b;
+function calculate(meal1, meal2, meal3) {
+    const dailyCalorieRequirement = 2200;
+    const totalCalories = meal1 + meal2 + meal3;
+    if (totalCalories > dailyCalorieRequirement) {
+        const excessCalories = totalCalories - dailyCalorieRequirement;
+        return `คุณกินเกิน ${excessCalories} แคลอรี่`;
+    }
+    else {
+        return `คุณยังเหลืออีก ${dailyCalorieRequirement - totalCalories} แคลอรี่ ที่ควรได้รับ`;
+    }
 }
-function addUser(data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield axios_1.default.post('https://jsonplaceholder.typicode.com/users', data);
-        return response.data;
-    });
-}
-exports.Utils = { addUser, add };
-//web browser (google.com) ---> (request) ---> google company server
-//                 <html>... (html)  <-- (respon) -----google company server
+exports.Utils = { calculate };
